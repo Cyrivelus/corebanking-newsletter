aef92eb99b23cf391a3eca73fb6d469cd4ca2587
Cyrivelus <cyrivelmars2013@gmail.com>
2026-03-24 18:11:15 +0100

    Fix: Hardcoding tenant ID in username for Supabase pooler

--- config/database.php
+++ config/database.php
@@ -4,26 +4,25 @@ return [
     'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
-        'pgsql' => [
-            'driver' => 'pgsql',
-            // Sur Vercel, l'URL complète contient le tenant-id dans le login.
-            // C'est la méthode la plus fiable.
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
-            'port' => env('DB_PORT', '6543'),
-            'database' => env('DB_DATABASE', 'postgres'),
-            'username' => env('DB_USERNAME', 'postgres.hwllvkdvhfrkajgxwdeo'),
-            'password' => env('DB_PASSWORD', 'fireFlame237KLMNOPQSDFG'),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'require',
-            'options' => [
-                // Désactivation du cache de connexion pour le Serverless
-                PDO::ATTR_PERSISTENT => false,
-            ],
-        ],
+      'pgsql' => [
+    'driver' => 'pgsql',
+    // On utilise l'URL complète si elle existe, c'est le plus fiable sur Vercel
+    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
+    'port' => env('DB_PORT', '6543'),
+    'database' => env('DB_DATABASE', 'postgres'),
+    // FORCE le format postgres.PROJECT_ID
+    'username' => env('DB_USERNAME', 'postgres.hwllvkdvhfrkajgxwdeo'),
+    'password' => env('DB_PASSWORD', 'fireFlame237KLMNOPQSDFG'),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'search_path' => 'public',
+    'sslmode' => 'require',
+    'options' => [
+        \PDO::ATTR_PERSISTENT => false,
+    ],
+],
     ],
 
     'migrations' => [
